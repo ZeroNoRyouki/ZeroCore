a89403586daef08043ef894b1b6abde99b01bcfb
ZeroNoRyouki <zeronoryouki@gmail.com>
2016-12-28 11:23:33 +0100

    added NBT files helpers

--- src/main/java/it/zerono/mods/zerocore/util/CodeHelper.java
+++ src/main/java/it/zerono/mods/zerocore/util/CodeHelper.java
@@ -2,8 +2,15 @@ package it.zerono.mods.zerocore.util;
 
 import net.minecraft.client.resources.I18n;
 import net.minecraft.launchwrapper.Launch;
+import net.minecraft.nbt.CompressedStreamTools;
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraftforge.fml.common.Loader;
 import net.minecraftforge.fml.common.ModContainer;
+
+import javax.annotation.Nullable;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 
 public final class CodeHelper {
 
@@ -35,6 +42,58 @@ public final class CodeHelper {
      */
     public static boolean runningInDevEnv() {
         return (Boolean)Launch.blackboard.get("fml.deobfuscatedEnvironment");
+    }
+
+    /**
+     * Load an NBTTagCompound from the given file
+     *
+     * @param file the file to read from
+     * @return the NBTTagCompound read from the file or null if, for whatever reason, the operation fails
+     */
+    @Nullable
+    public static NBTTagCompound nbtLoadFrom(final File file) {
+
+        if (null == file)
+            throw new IllegalArgumentException("The file to read from cannot be null");
+
+        if (file.exists()) {
+
+            try (FileInputStream stream = new FileInputStream(file)) {
+                return CompressedStreamTools.readCompressed(stream);
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        }
+
+        return null;
+    }
+
+    /**
+     * Save an NBTTagCompound to the given file
+     *
+     * @param file the file to write the data to
+     * @param data the data to store in the file
+     * @return true if the operation succeeded, false otherwise
+     */
+    @Nullable
+    public static boolean nbtSaveTo(final File file, final NBTTagCompound data) {
+
+        if (null == file)
+            throw new IllegalArgumentException("The file to write to cannot be null");
+
+        if (null == data)
+            throw new IllegalArgumentException("The data to write cannot be null");
+
+        try (FileOutputStream stream = new FileOutputStream(file)) {
+
+            CompressedStreamTools.writeCompressed(data, stream);
+            return true;
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+        return false;
     }
 
 }
