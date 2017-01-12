e4a43ee224e72c3c6bf47b17c72f384171b3c47b
ZeroNoRyouki <zeronoryouki@gmail.com>
2017-01-12 22:36:18 +0100

    Update Multiblock API version to 1.10.2-0.0.2 Bumped version to 1.10.2-0.1.0.1

--- src/main/java/it/zerono/mods/zerocore/api/multiblock/package-info.java
+++ src/main/java/it/zerono/mods/zerocore/api/multiblock/package-info.java
@@ -1,4 +1,4 @@
-@API(apiVersion = "1.9.4-0.0.1", owner = "zerocore", provides = "zerocore|API|multiblock")
+@API(apiVersion = "1.10.2-0.0.2", owner = "zerocore", provides = "zerocore|API|multiblock")
 package it.zerono.mods.zerocore.api.multiblock;
 
 import net.minecraftforge.fml.common.API;
