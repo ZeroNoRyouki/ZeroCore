4fe6a816cf0208e409837590beaf871a467d3ea7
ZeroNoRyouki <zeronoryouki@gmail.com>
2017-01-17 18:52:48 +0100

    Changed items name to lowercase and added a missing mappings event handler

--- src/main/java/it/zerono/mods/zerocore/internal/ZeroCore.java
+++ src/main/java/it/zerono/mods/zerocore/internal/ZeroCore.java
@@ -6,8 +6,10 @@ import it.zerono.mods.zerocore.lib.IModInitializationHandler;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLMissingMappingsEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.registry.GameRegistry;
 
 @Mod(modid = References.MOD_ID, name = References.MOD_NAME, acceptedMinecraftVersions = "",
         dependencies = "required-after:Forge", version = "0.0.0.0")
@@ -33,6 +35,21 @@ public final class ZeroCore implements IModInitializationHandler {
     @Mod.EventHandler
     public void onPostInit(FMLPostInitializationEvent event) {
         ZeroCore.s_proxy.onPostInit(event);
+    }
+
+    @Mod.EventHandler
+    public void onMissingMapping(FMLMissingMappingsEvent event) {
+
+        for (FMLMissingMappingsEvent.MissingMapping mapping : event.get()) {
+
+            String oldName = mapping.resourceLocation.getResourcePath().toLowerCase();
+
+            if (GameRegistry.Type.ITEM == mapping.type && "debugtool".equals(oldName)) {
+
+                mapping.remap(ZeroItems.debugTool);
+                return;
+            }
+        }
     }
 
     @Mod.Instance
