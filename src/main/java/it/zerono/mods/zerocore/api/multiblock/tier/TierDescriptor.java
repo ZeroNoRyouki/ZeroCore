cefdda3e1b7728b1f95e4ced2047b71faa01db33
ZeroNoRyouki <zeronoryouki@gmail.com>
2017-05-18 19:51:38 +0200

    Multiblock API: added tiers support

---
+++ src/main/java/it/zerono/mods/zerocore/api/multiblock/tier/TierDescriptor.java
@@ -0,0 +1,20 @@
+package it.zerono.mods.zerocore.api.multiblock.tier;
+
+import it.zerono.mods.zerocore.lib.block.properties.IPropertyValue;
+import javax.annotation.Nonnull;
+
+public abstract class TierDescriptor<Tier extends Enum<Tier> & IPropertyValue> {
+
+    public final Tier Tier;
+    public final int MaxSizeX;
+    public final int MaxSizeY;
+    public final int MaxSizeZ;
+
+    TierDescriptor(@Nonnull final Tier tier, final int maxX, final int maxY, final int maxZ) {
+
+        this.Tier = tier;
+        this.MaxSizeX = maxX;
+        this.MaxSizeY = maxY;
+        this.MaxSizeZ = maxZ;
+    }
+}
---
+++ src/main/java/it/zerono/mods/zerocore/api/multiblock/tier/package-info.java
@@ -0,0 +1,4 @@
+@API(apiVersion = "1.10.2-0.0.2", owner = "zerocore", provides = "zerocore|API|multiblock|tier")
+package it.zerono.mods.zerocore.api.multiblock.tier;
+
+import net.minecraftforge.fml.common.API;
