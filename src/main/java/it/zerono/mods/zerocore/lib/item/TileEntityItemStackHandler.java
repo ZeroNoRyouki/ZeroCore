405b00c9aa0447bc4713b21af1943ce150327b56
ZeroNoRyouki <zeronoryouki@gmail.com>
2017-05-24 18:59:42 +0200

    Reformatted code

--- src/main/java/it/zerono/mods/zerocore/lib/item/TileEntityItemStackHandler.java
+++ src/main/java/it/zerono/mods/zerocore/lib/item/TileEntityItemStackHandler.java
@@ -7,23 +7,25 @@ import net.minecraftforge.items.ItemStackHandler;
 public class TileEntityItemStackHandler extends ItemStackHandler {
 
     public TileEntityItemStackHandler(TileEntity linkedTileEntity) {
+
         super(1);
         this._linkedTE = linkedTileEntity;
     }
 
     public TileEntityItemStackHandler(TileEntity linkedTileEntity, int size) {
+
         super(size);
         this._linkedTE = linkedTileEntity;
     }
 
     public TileEntityItemStackHandler(TileEntity linkedTileEntity, ItemStack[] stacks) {
+
         super(stacks);
         this._linkedTE = linkedTileEntity;
     }
 
     @Override
     protected void onContentsChanged(int slot) {
-
         this._linkedTE.markDirty();
     }
 
