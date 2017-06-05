81ecea04d172fef272c90445c255deabae6190e3
ZeroNoRyouki <zeronoryouki@gmail.com>
2017-06-05 22:10:17 +0200

    Added status message support

--- src/main/java/it/zerono/mods/zerocore/internal/common/CommonProxy.java
+++ src/main/java/it/zerono/mods/zerocore/internal/common/CommonProxy.java
@@ -4,12 +4,18 @@ import it.zerono.mods.zerocore.api.multiblock.IMultiblockRegistry;
 import it.zerono.mods.zerocore.internal.common.init.ZeroItems;
 import it.zerono.mods.zerocore.lib.IGameObject;
 import it.zerono.mods.zerocore.lib.IModInitializationHandler;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.item.Item;
+import net.minecraft.network.play.server.SPacketChat;
+import net.minecraft.util.text.ITextComponent;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.registry.GameRegistry;
+
+import javax.annotation.Nonnull;
 
 public class CommonProxy implements IModInitializationHandler {
 
@@ -40,6 +46,12 @@ public class CommonProxy implements IModInitializationHandler {
 
     @Override
     public void onPostInit(FMLPostInitializationEvent event) {
+    }
+
+    public void sendPlayerStatusMessage(@Nonnull final EntityPlayer player, @Nonnull final ITextComponent message) {
+
+        if (player instanceof EntityPlayerMP)
+            ((EntityPlayerMP)player).connection.sendPacket(new SPacketChat(message, (byte)2));
     }
 
     private static MultiblockEventHandler s_multiblockHandler = null;
