16e8c4b8724d0bcbfffdb1bb0b231e2122a84f3d
ZeroNoRyouki <zeronoryouki@gmail.com>
2017-06-05 22:09:03 +0200

    Added status message support

--- src/main/java/it/zerono/mods/zerocore/internal/client/ClientProxy.java
+++ src/main/java/it/zerono/mods/zerocore/internal/client/ClientProxy.java
@@ -3,9 +3,17 @@ package it.zerono.mods.zerocore.internal.client;
 import it.zerono.mods.zerocore.internal.common.CommonProxy;
 import it.zerono.mods.zerocore.lib.IGameObject;
 import it.zerono.mods.zerocore.lib.client.VersionChecker;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.entity.EntityPlayerSP;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.item.Item;
+import net.minecraft.network.play.server.SPacketChat;
+import net.minecraft.util.text.ITextComponent;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+import javax.annotation.Nonnull;
 
 public class ClientProxy extends CommonProxy {
 
@@ -24,5 +32,12 @@ public class ClientProxy extends CommonProxy {
 
         MinecraftForge.EVENT_BUS.register(VersionChecker.class);
         //VersionChecker.scheduleCheck("http://ws.zerono.it/check.ashx");
+    }
+
+    @Override
+    public void sendPlayerStatusMessage(@Nonnull final EntityPlayer player, @Nonnull final ITextComponent message) {
+
+        if (player instanceof EntityPlayerSP)
+            Minecraft.getMinecraft().ingameGUI.setRecordPlayingMessage(message.getUnformattedText());
     }
 }
