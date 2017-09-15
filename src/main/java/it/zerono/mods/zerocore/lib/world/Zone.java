7076972304dc52820fbd5601048890780f9d07f7
ZeroNoRyouki <zeronoryouki@gmail.com>
2017-09-15 16:19:05 +0200

    More work on the spawn exclusion zone

--- src/main/java/it/zerono/mods/zerocore/lib/world/Zone.java
+++ src/main/java/it/zerono/mods/zerocore/lib/world/Zone.java
@@ -1,10 +1,9 @@
 package it.zerono.mods.zerocore.lib.world;
 
+import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
 
-public abstract class Zone {
-
-    public abstract boolean contains(final BlockPos position);
+public abstract class Zone implements Comparable<Zone> {
 
     public static Zone rectangular(final BlockPos minCoords, final BlockPos maxCoords) {
         return new RectangularCuboidZone(minCoords, maxCoords);
@@ -12,6 +11,19 @@ public abstract class Zone {
 
     public static Zone sphere(final BlockPos center, int radius) {
         return new SphereZone(center, radius);
+    }
+
+    public abstract boolean contains(final BlockPos position);
+
+    public abstract AxisAlignedBB getBoundingBox();
+
+    @Override
+    public int compareTo(Zone zone) {
+
+        final AxisAlignedBB myBB = this.getBoundingBox();
+        final AxisAlignedBB hisBB = zone.getBoundingBox();
+
+        return 0;
     }
 
     private static class RectangularCuboidZone extends Zone {
@@ -28,6 +40,25 @@ public abstract class Zone {
             return (this._minCoords.getX() <= position.getX() && position.getX() <= this._maxCoords.getX()) &&
                    (this._minCoords.getY() <= position.getY() && position.getY() <= this._maxCoords.getY()) &&
                    (this._minCoords.getZ() <= position.getZ() && position.getZ() <= this._maxCoords.getZ());
+        }
+
+        @Override
+        public AxisAlignedBB getBoundingBox() {
+            return new AxisAlignedBB(this._minCoords, this._maxCoords);
+        }
+
+        @Override
+        public boolean equals(Object o) {
+
+            if (this == o)
+                return true;
+
+            if (!(o instanceof RectangularCuboidZone))
+                return false;
+
+            RectangularCuboidZone other = (RectangularCuboidZone)o;
+
+            return this._minCoords.equals(other._minCoords) && this._maxCoords.equals(other._maxCoords);
         }
 
         private final BlockPos _minCoords;
@@ -49,6 +80,31 @@ public abstract class Zone {
                     ((position.getY() - this._center.getY()) * (position.getY() - this._center.getY())) +
                     ((position.getZ() - this._center.getZ()) * (position.getZ() - this._center.getZ()))) <=
                     this._radius * this._radius;
+        }
+
+        @Override
+        public AxisAlignedBB getBoundingBox() {
+
+            final int centerX = this._center.getX();
+            final int centerY = this._center.getY();
+            final int centerZ = this._center.getZ();
+
+            return new AxisAlignedBB(centerX - this._radius, centerY - this._radius, centerZ - this._radius,
+                                     centerX + this._radius - 1, centerY + this._radius - 1, centerZ + this._radius - 1);
+        }
+
+        @Override
+        public boolean equals(Object o) {
+
+            if (this == o)
+                return true;
+
+            if (!(o instanceof SphereZone))
+                return false;
+
+            SphereZone other = (SphereZone)o;
+
+            return this._radius == other._radius && this._center.equals(other._center);
         }
 
         private final BlockPos _center;
---
+++ src/main/java/it/zerono/mods/zerocore/lib/world/ZonePositionComparator.java
@@ -0,0 +1,11 @@
+package it.zerono.mods.zerocore.lib.world;
+
+import java.util.Comparator;
+
+public class ZonePositionComparator implements Comparator<Zone> {
+
+    @Override
+    public int compare(Zone zone1, Zone zone2) {
+        return 0;
+    }
+}
