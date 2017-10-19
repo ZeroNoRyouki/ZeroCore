94d5294a7b172c23801ba47621be371f86bc58a1
ZeroNoRyouki <zeronoryouki@gmail.com>
2017-10-19 09:25:02 +0200

    Added createResource() method to the ZeroCore class

--- src/main/java/it/zerono/mods/zerocore/internal/ZeroCore.java
+++ src/main/java/it/zerono/mods/zerocore/internal/ZeroCore.java
@@ -2,10 +2,13 @@ package it.zerono.mods.zerocore.internal;
 
 import it.zerono.mods.zerocore.internal.common.CommonProxy;
 import it.zerono.mods.zerocore.internal.common.init.ObjectsHandler;
+import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.SidedProxy;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+
+import javax.annotation.Nonnull;
 
 @Mod(modid = References.MOD_ID, name = References.MOD_NAME, acceptedMinecraftVersions = "",
         dependencies = "required-after:Forge", version = "0.0.0.0")
@@ -21,6 +24,10 @@ public final class ZeroCore {
             s_modLogger = LogManager.getLogger(References.MOD_ID);
 
         return s_modLogger;
+    }
+
+    public static ResourceLocation createResourceLocation(@Nonnull final String path) {
+        return new ResourceLocation(References.MOD_ID, path);
     }
 
     public ZeroCore() {
