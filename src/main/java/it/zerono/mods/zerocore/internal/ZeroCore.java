3ce7e43731a0354f330b9924929ee317a2b82968
ZeroNoRyouki <zeronoryouki@gmail.com>
2017-10-23 15:39:59 +0200

    Added back FML events handler to call the GameObjectsHandler()

--- src/main/java/it/zerono/mods/zerocore/internal/ZeroCore.java
+++ src/main/java/it/zerono/mods/zerocore/internal/ZeroCore.java
@@ -2,9 +2,14 @@ package it.zerono.mods.zerocore.internal;
 
 import it.zerono.mods.zerocore.internal.common.CommonProxy;
 import it.zerono.mods.zerocore.internal.common.init.ObjectsHandler;
+import it.zerono.mods.zerocore.lib.IModInitializationHandler;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.SidedProxy;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLMissingMappingsEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -12,7 +17,7 @@ import javax.annotation.Nonnull;
 
 @Mod(modid = References.MOD_ID, name = References.MOD_NAME, acceptedMinecraftVersions = "",
         dependencies = "required-after:Forge", version = "0.0.0.0")
-public final class ZeroCore {
+public final class ZeroCore implements IModInitializationHandler {
 
     public static CommonProxy getProxy() {
         return s_proxy;
@@ -31,6 +36,29 @@ public final class ZeroCore {
     }
 
     public ZeroCore() {
+    }
+
+    @Mod.EventHandler
+    @Override
+    public void onPreInit(FMLPreInitializationEvent event) {
+        this._objectsHandler.onPreInit(event);
+    }
+
+    @Mod.EventHandler
+    @Override
+    public void onInit(FMLInitializationEvent event) {
+        this._objectsHandler.onInit(event);
+    }
+
+    @Mod.EventHandler
+    @Override
+    public void onPostInit(FMLPostInitializationEvent event) {
+        this._objectsHandler.onPostInit(event);
+    }
+
+    @Mod.EventHandler
+    public void onMissingMapping(FMLMissingMappingsEvent event) {
+        this._objectsHandler.onMissinMappings(event);
     }
 
     @Mod.Instance
