a58e25eaffcf26a123bd636316dcd3d3cdfe72b2
ZeroNoRyouki <zeronoryouki@gmail.com>
2018-03-25 13:40:15 +0200

    IThreadListener helper functions

--- src/main/java/it/zerono/mods/zerocore/internal/common/CommonProxy.java
+++ src/main/java/it/zerono/mods/zerocore/internal/common/CommonProxy.java
@@ -4,11 +4,14 @@ import it.zerono.mods.zerocore.api.multiblock.IMultiblockRegistry;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.network.play.server.SPacketChat;
+import net.minecraft.util.IThreadListener;
 import net.minecraft.util.text.ChatType;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.FMLCommonHandler;
 
 import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 
 public class CommonProxy {
 
@@ -24,6 +27,14 @@ public class CommonProxy {
 
         if (player instanceof EntityPlayerMP)
             ((EntityPlayerMP)player).connection.sendPacket(new SPacketChat(message, ChatType.GAME_INFO));
+    }
+
+    public IThreadListener getClientThreadListener() {
+        return null;
+    }
+
+    public IThreadListener getServerThreadListener() {
+        return FMLCommonHandler.instance().getMinecraftServerInstance();
     }
 
     private static MultiblockEventHandler s_multiblockHandler = null;
